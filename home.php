ba761642f55ff1b497f95f74d5f162634fda9070
faizannaveed <57486515+faizannaveed@users.noreply.github.com>
2022-03-11 21:51:47 +0000

    updated file name

--- home.php
+++ home.php
@@ -9,7 +9,7 @@ if(!isset($_SESSION['username']))
 <?php include 'header.php';?>
 <div id="home-container">
   <div class="top left">
-  <button class="home-button button3" onclick="document.location='readevents.php'">Events page</button>
+  <button class="home-button button3" onclick="document.location='readevent.php'">Events page</button>
   </div>
   <div class="top right">
   <button class="home-button button3" onclick="document.location='#'">Products page</button>
